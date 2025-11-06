bdf908ce71068ee9ccafa408310a5fdf410b618e
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-11-06 16:22:15 +0900

    [FE](feat) - 실시간 인기 장소 조회 API 연동 (#6)
    * chore: og-image 변경
    
    * feat: 제주 실시간 관광객 유동인구 API 연동

--- apps/web/src/app/(with-navigation)/home/page.tsx
+++ apps/web/src/app/(with-navigation)/home/page.tsx
@@ -3,21 +3,30 @@ import Link from "next/link";
 import Search from "@/assets/search.svg";
 import FloatingNavButton from "@/components/common/button/floating-nav-button";
 import Header from "@/components/common/header";
+import { JEJU } from "@/constants/jeju";
 import { ROAD } from "@/constants/road";
 import { BaseResponseDTO } from "@/models";
+import { JejuRealtimeResponseDTO } from "@/models/jeju";
 import { RoadResponseDTO } from "@/models/road";
 import HomePage from "@/page/home";
 import { serverApi } from "@/services/api";
 import { getParams } from "@/utils/params";
 
+import ky from "ky";
+
 interface HomeProps {
   searchParams: Promise<{
     category: string;
+    realtime: string;
   }>;
 }
 
+const JEJU_DATA_API_URL = process.env.JEJU_DATA_API_URL!;
+
 export default async function Home({ searchParams }: HomeProps) {
-  const { category: categoryId } = await searchParams;
+  const { category: categoryId, realtime } = await searchParams;
+
+  const realtimeType = realtime ?? "tour";
 
   const params = getParams({ categoryId });
   const promisedResponse = serverApi.get<BaseResponseDTO<RoadResponseDTO[]>>(
@@ -28,6 +37,15 @@ export default async function Home({ searchParams }: HomeProps) {
       },
     }
   );
+
+  const realtimeResponse = ky
+    .get<JejuRealtimeResponseDTO>(`${JEJU_DATA_API_URL}/kor/${realtimeType}/ranking.do`, {
+      next: {
+        tags: [JEJU.REALTIME, realtimeType],
+        revalidate: 60 * 1000,
+      },
+    })
+    .json();
 
   return (
     <>
@@ -40,7 +58,7 @@ export default async function Home({ searchParams }: HomeProps) {
         }
         sticky
       />
-      <HomePage promisedResponse={promisedResponse} />
+      <HomePage promisedResponse={promisedResponse} realtimeResponse={realtimeResponse} />
       <FloatingNavButton />
     </>
   );
